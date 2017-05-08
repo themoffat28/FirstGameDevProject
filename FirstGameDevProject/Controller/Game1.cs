48ebe87430c7b875405fd0b24e263c9a7868fc4c
themoffat28 <bthemoffat@gmail.com>
2017-05-08 09:08:22 -0600

    Added ability to shoot

--- FirstGameDevProject/Controller/Game1.cs
+++ FirstGameDevProject/Controller/Game1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-            
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -45,6 +45,13 @@ namespace FirstGameDevProject.Controller
 		// A random number generator
 		Random random;
 
+		Texture2D projectileTexture;
+		List<Projectile> projectiles;
+
+		// The rate of fire of the player laser
+		TimeSpan fireTime;
+		TimeSpan previousFireTime;
+
 
 		public Game1 ()
 		{
@@ -81,6 +88,11 @@ namespace FirstGameDevProject.Controller
 			// Initialize our random number generator
 			random = new Random();
 
+			projectiles = new List<Projectile>();
+
+			// Set the laser to fire every quarter second
+			fireTime = TimeSpan.FromSeconds(.15f);
+
 			base.Initialize();
 		}
 
@@ -110,6 +122,8 @@ namespace FirstGameDevProject.Controller
 			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
 
 			enemyTexture = Content.Load<Texture2D>("mineAnimation");
+
+			projectileTexture = Content.Load<Texture2D>("Texture/laser");
 
 			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 
@@ -148,6 +162,13 @@ namespace FirstGameDevProject.Controller
 
 			// Update the enemies
 			UpdateEnemies(gameTime);
+
+			// Update the collision
+			UpdateCollision();
+
+			// Update the projectiles
+			UpdateProjectiles();
+
 		}
 
 		private void UpdatePlayer (GameTime gameTime)
@@ -177,6 +198,16 @@ namespace FirstGameDevProject.Controller
 			currentGamePadState.DPad.Down == ButtonState.Pressed)
 			{
 				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Fire only every interval we set as the fireTime
+			if (gameTime.TotalGameTime - previousFireTime > fireTime)
+			{
+				// Reset our current time
+				previousFireTime = gameTime.TotalGameTime;
+
+				// Add the projectile, but add it to the front and center of the player
+				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
 			}
 
 			// Make sure that the player does not go out of bounds
@@ -229,6 +260,93 @@ namespace FirstGameDevProject.Controller
 			}
 		}
 
+		private void UpdateCollision ()
+		{
+			// Use the Rectangle's built-in intersect function to 
+			// determine if two objects are overlapping
+			Rectangle rectangle1;
+			Rectangle rectangle2;
+
+			// Only create the rectangle once for the player
+			rectangle1 = new Rectangle((int)player.Position.X,
+			(int)player.Position.Y,
+			player.Width,
+			player.Height);
+
+			// Do the collision between the player and the enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				rectangle2 = new Rectangle((int)enemies[i].Position.X,
+				(int)enemies[i].Position.Y,
+				enemies[i].Width,
+				enemies[i].Height);
+
+				// Determine if the two objects collided with each
+				// other
+				if (rectangle1.Intersects(rectangle2))
+				{
+					// Subtract the health from the player based on
+					// the enemy damage
+					player.Health -= enemies[i].Damage;
+
+					// Since the enemy collided with the player
+					// destroy it
+					enemies[i].Health = 0;
+
+					// If the player health is less than zero we died
+					if (player.Health <= 0)
+						player.Active = false;
+				}
+
+
+			}
+
+			// Projectile vs Enemy Collision
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				for (int j = 0; j < enemies.Count; j++)
+				{
+					// Create the rectangles we need to determine if we collided with each other
+					rectangle1 = new Rectangle((int)projectiles[i].Position.X -
+					projectiles[i].Width / 2, (int)projectiles[i].Position.Y -
+					projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
+
+					rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2,
+					(int)enemies[j].Position.Y - enemies[j].Height / 2,
+					enemies[j].Width, enemies[j].Height);
+
+					// Determine if the two objects collided with each other
+					if (rectangle1.Intersects(rectangle2))
+					{
+						enemies[j].Health -= projectiles[i].Damage;
+						projectiles[i].Active = false;
+					}
+				}
+			}
+		}
+
+		private void AddProjectile (Vector2 position)
+		{
+			Projectile projectile = new Projectile();
+			projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+			projectiles.Add(projectile);
+		}
+
+		private void UpdateProjectiles ()
+		{
+			// Update the Projectiles
+			for (int i = projectiles.Count - 1; i >= 0; i--)
+			{
+				projectiles[i].Update();
+
+				if (projectiles[i].Active == false)
+				{
+					projectiles.RemoveAt(i);
+				}
+
+			}
+		}
+
 		/// <summary>
 		/// This is called when the game should draw itself.
 		/// </summary>
@@ -250,6 +368,12 @@ namespace FirstGameDevProject.Controller
 			for (int i = 0; i < enemies.Count; i++)
 			{
 				enemies[i].Draw(spriteBatch);
+			}
+
+			// Draw the Projectiles
+			for (int i = 0; i < projectiles.Count; i++)
+			{
+				projectiles[i].Draw(spriteBatch);
 			}
 
 			// Draw the Player
